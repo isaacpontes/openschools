2338ccc9ebd291381b269bb0afc793e4eade65ae
Isaac L. F. Pontes <isaaclfp@outlook.com>
2021-09-09 16:48:13 -0300

    Converted database to class

--- config/database.js
+++ config/database.js
@@ -1,16 +1,36 @@
 const mongoose = require('mongoose');
 const seedAdmin = require('./seed');
 
-mongoose.Promise = global.Promise;
+class Database {
+  constructor ({ host, port, name, username, password }, connection) {
+    this.host = host;
+    this.port = port;
+    this.name = name;
+    this.username = username;
+    this.password = password;
+    this.connection = connection;
+  }
 
-mongoose.connect('mongodb://localhost:27017/openschools', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
-  .then((mongo) => {
-    console.log('Successfully connected to MongoDB.');
-    seedAdmin(mongo.connection.collections.users);
-  })
-  .catch((err) => console.log(err));
+  async connect() {
+    const connectionString = `mongodb://${this.username}:${this.password}@${this.host}:${this.port}/${this.name}`;
+
+    try {
+      const mongo = await mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      });
+
+      this.connection = mongo.connection;
+      console.log('Successfully connected to MongoDB.');
+
+      seedAdmin(mongo.connection.collections.users);
+
+    } catch (error) {
+      console.log(error);
+    }    
+  }
+}
+
+module.exports = Database;
